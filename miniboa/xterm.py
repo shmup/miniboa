500f9796b59c0cf9e73d8877cd9e64dc88b047c8
jimstorch <jimstorch@436d515c-df73-11de-a155-4d945b6d75ec>
2010-01-31 21:08:00 +0000

    Changed ANSI caret codes, please see the wiki.

--- miniboa/xterm.py
+++ miniboa/xterm.py
@@ -24,13 +24,13 @@ import re
 _ANSI_CODES = (
 
     # Note: order here matters to keep '^b' from clobbering '^bb'
-    ( '^kb', '\x1b[40m' ),          # black background
-    ( '^rb', '\x1b[41m' ),          # red background
-    ( '^gb', '\x1b[42m' ),          # green background
-    ( '^yb', '\x1b[43m' ),          # yellow background
-    ( '^bb', '\x1b[44m' ),          # blue background
-    ( '^mb', '\x1b[45m' ),          # magenta background
-    ( '^cb', '\x1b[46m' ),          # cyan background
+    ( '^0', '\x1b[40m' ),          # black background
+    ( '^1', '\x1b[41m' ),          # red background
+    ( '^2', '\x1b[42m' ),          # green background
+    ( '^3', '\x1b[43m' ),          # yellow background
+    ( '^4', '\x1b[44m' ),          # blue background
+    ( '^5', '\x1b[45m' ),          # magenta background
+    ( '^6', '\x1b[46m' ),          # cyan background
     ( '^k', '\x1b[22;30m' ),        # black
     ( '^K', '\x1b[1;30m' ),         # bright black (grey)
     ( '^r', '\x1b[22;31m' ),        # red
@@ -48,13 +48,13 @@ _ANSI_CODES = (
     ( '^w', '\x1b[22;37m' ),        # white
     ( '^W', '\x1b[1;37m' ),         # bright white
     ( '^d', '\x1b[39m' ),           # default (should be white on black)
-    ( '^i', '\x1b[7m' ),            # inverse text on
-    ( '^I', '\x1b[27m' ),           # inverse text off
-    ( '^^', '\x1b[0m' ),            # reset all
-    ( '^_', '\x1b[4m' ),            # underline on
-    ( '^-', '\x1b[24m' ),           # underline off
+    ( '^I', '\x1b[7m' ),            # inverse text on
+    ( '^i', '\x1b[27m' ),           # inverse text off
+    ( '^~', '\x1b[0m' ),            # reset all
+    ( '^U', '\x1b[4m' ),            # underline on
+    ( '^u', '\x1b[24m' ),           # underline off
     ( '^!', '\x1b[1m' ),            # bold on
-    ( '^1', '\x1b[22m'),            # bold off
+    ( '^.', '\x1b[22m'),            # bold off
     ( '^s', '\x1b[2J'),             # clear screen
     ( '^l', '\x1b[2K'),             # clear to end of line
     )
@@ -64,9 +64,11 @@ def strip_caret_codes(text):
     """
     Strip out any caret codes from a string.
     """
+    ## temporarily escape out ^^
+    text = text.replace('^^', '___^___')
     for token, throwaway in _ANSI_CODES:
         text = text.replace(token, '')
-    return text
+    return text.replace('___^___', '^')
 
 
 def colorize(text, ansi=True):
@@ -75,8 +77,10 @@ def colorize(text, ansi=True):
     otherwise, simply strip them out.
     """
     if ansi:
+        text = text.replace('^^', '___^___')        
         for token, code in _ANSI_CODES:
             text = text.replace(token, code)
+        text = text.replace('___^___', '^')
     else:
         text = strip_caret_codes(text)
     return text
