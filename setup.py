8ca0a6bb643d8d9815e04d641be6fb95682f31d4
Jared Miller <jtmiller@gmail.com>
2016-01-19 21:00:00 -0500

    adding a pypi setup config and pointing to README

---
+++ setup.py
@@ -0,0 +1,14 @@
+setup(
+    name='miniboa',
+    version='0.1',
+    packages=['miniboa'],
+    description='A bare-bones Telnet server',
+    author='Jim Storch',
+    author_email='jimstorch@gmail.com',
+    maintainer='Jared Miller',
+    maintainer_email='jtmiller@gmail.com',
+    url='https://github.com/shmup/miniboa',
+    download_url='https://github.com/shmup/miniboa/archive/v0.1.tar.gz',
+    keywords=['mud', 'tcp', 'telnet']
+)
+
