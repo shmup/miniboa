df0468d189c9aaddcbe3ee35be8b6627ce6b88fe
Jared Miller <jared.miller@northpointeinc.com>
2016-01-21 16:19:23 -0500

    Adding a world, zone, rooms, and player

--- examples/mud_demo.py
+++ examples/mud_demo.py
@@ -2,12 +2,24 @@
 
 from miniboa import TelnetServer
 
+world = """
+~~~...........^^^...
+~~~~.........^^^....
+~~~~~~........^^^^..
+~~~~~...........^^^^
+~~~.................
+....................
+....................
+....................
+"""
+
 
 class Player:
     def __init__(self, client):
         self.client = client
         self.state = 'new'
         self.name = None
+        self.queue = []
 
     def process_input(self):
         command = None
@@ -25,12 +37,17 @@ class Player:
                 self.state = 'live'
         elif self.state == 'live':
             if command is not None:
-                print(command)
+                self.queue.append(command)
+
+            if len(self.queue):
+                return self.queue.pop()
 
 
 class World:
     def __init__(self):
         self.players = {}
+        self.zone = Zone('earth')
+        self.updates = []
 
     def add_player(self, client):
         print("Adding player: {}".format(client.fileno))
@@ -41,9 +58,62 @@ class World:
         del self.players[client.fileno]
 
     def poll(self):
-        for _, player in self.players.items():
-            player.process_input()
-        pass
+        for key, player in self.players.items():
+            update = player.process_input()
+            if update:
+                self.updates.append((key, update))
+
+        self.update()
+
+    def update(self):
+        updates = self.updates
+        self.updates = []
+
+        for update in updates:
+            (key, cmd) = update
+            cmd = cmd.strip().split(' ')
+            command = cmd[0]
+            modifiers = cmd[1:]
+            self.process_update(key, command, modifiers)
+
+    def process_update(self, key, command, modifiers):
+        if command == 'look':
+            self.zone.display()
+
+
+class Zone:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+        self.build(world)
+
+    def build(self, data):
+        lines = data.split()
+        for line in lines:
+            map_row = []
+            for c in line:
+                map_row.append(Room(c))
+            self.rooms.append(map_row)
+
+    def display(self):
+        for row in self.rooms:
+            print("".join([x.color + x.symbol for x in row]))
+
+
+class Room:
+    def __init__(self, symbol, color=None):
+        self.symbol = symbol
+        self.players = []
+        self.items = []
+        self.color = color
+
+        if self.color is None:
+            if symbol == ".":
+                self.color = '\033[0;32m'
+            elif symbol == "~":
+                self.color = '\033[0;34m'
+            else:
+                self.color = '\033[0;31m'
 
 
 if __name__ == "__main__":
