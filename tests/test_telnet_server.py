8eaf64f8c92786c6f94f258848d1efb4101ba42a
Jared Miller <jtmiller@gmail.com>
2016-01-20 22:45:45 -0500

    Test that we have a connected client

--- tests/test_telnet_server.py
+++ tests/test_telnet_server.py
@@ -14,13 +14,18 @@ def test_telnet_server():
         on_connect=lambda x: CLIENTS.append(x),
         timeout=.05)
 
-    bart = socket.socket()
-    bart.connect(('127.0.0.1', 7777))
+    client = socket.socket()
+    client.connect(('127.0.0.1', 7777))
     server.poll()
+
+    # test that we have one connected client
+    assert len(CLIENTS) == 2
+
     CLIENTS[0].send("test")
     server.poll()
-    data = bart.recv(4)
+    data = client.recv(4)
 
+    # test that we received the correct data
     if PYTHON_2:
         assert data == "test"
     else:
