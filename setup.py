5700b7eb358ddb13969f4a1db67b3c676e944dc4
Jared Miller <jtmiller@gmail.com>
2016-01-20 20:52:03 -0500

    excluding some dirs

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
+from setuptools import setup, find_packages
+
 setup(
     name='miniboa',
     version='0.1',
-    packages=['miniboa'],
+    packages=find_packages(exclude=['docs', 'tests', 'examples']),
     description='A bare-bones Telnet server',
     author='Jim Storch',
     author_email='jimstorch@gmail.com',
@@ -9,6 +11,7 @@ setup(
     maintainer_email='jtmiller@gmail.com',
     url='https://github.com/shmup/miniboa',
     download_url='https://github.com/shmup/miniboa/archive/v0.1.tar.gz',
-    keywords=['mud', 'tcp', 'telnet']
+    keywords=['mud', 'tcp', 'telnet'],
+    license='Apache 2'
 )
 
