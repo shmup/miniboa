e8278288dc9934de80a51b302360fa4d1aaaaaab
Jared Miller <jtmiller@gmail.com>
2016-09-05 12:50:20 -0400

    putting more information in miniboa __init__.py

---
+++ src/miniboa/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from .async import TelnetServer
+
+__title__ = "miniboa"
+__description__ = "Asynchronous, single-threaded, poll-based Telnet server"
+__uri__ = "https://github.com/shmup/miniboa"
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "Jared Miller"
+__email__ = "jtmiller@gmail.com"
+
+__license__ = "Apache 2"
+__copyright__ = "Copyright (c) 2016 Jared Miller"
+
+__all__ = ['TelnetServer', ]
