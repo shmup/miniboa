9febfdc9b0c3a46fa0358830c56595ce7f43cd95
Jared Miller <jared@smell.flowers>
2022-09-18 22:31:00 -0400

    Update version

--- src/miniboa/__init__.py
+++ src/miniboa/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from .mbasync import TelnetServer
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 
 __title__ = "miniboa"
 __description__ = "Asynchronous, single-threaded, poll-based Telnet server"
@@ -9,9 +9,9 @@ __uri__ = "https://github.com/shmup/miniboa"
 __doc__ = __description__ + " <" + __uri__ + ">"
 
 __author__ = "Jared Miller"
-__email__ = "jtmiller@gmail.com"
+__email__ = "jared@smell.flowers"
 
 __license__ = "Apache 2"
-__copyright__ = "Copyright (c) 2018 Jared Miller"
+__copyright__ = "Copyright (c) 2022 Jared Miller"
 
 __all__ = ['TelnetServer', ]
